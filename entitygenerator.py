4ef104d59d3c6354043a5a598d640bc85c666b12
Karol Wach <karol@wach.me>
2013-10-02 05:38:45 +0200

    Associations support

--- entitygenerator.py
+++ entitygenerator.py
@@ -5,6 +5,7 @@ Created on 08-08-2013
 '''
 
 from os import makedirs
+from umlparser import AttributeDef
 
 class ClassMeta(object):
     def __init__(self, classDef=None, discr=None):
@@ -50,6 +51,7 @@ class EntityGenerator(object):
                     base.discr.append(meta.classDef.name)
                     break;
                 baseName = base.classDef.parent
+            meta.classDef.attributes = { n.name : n for n in meta.classDef.attributes}
         
         for meta in metas.values():
             try:
@@ -57,38 +59,29 @@ class EntityGenerator(object):
             except:
                 pass
             fp = open(self.path + '/' + meta.classDef.name + '.php', 'w')
-            fp.write(self.generateClass(meta))
+            fp.write(self.generateClass(metas, meta))
             fp.close()
 
+    def _genNamespace(self, metas, meta):
+        return "namespace {ns};".format(ns=self.namespace)
+    
+    def _genDiscrMap(self, metas, meta):
+        return ', '.join([ '"{n}" = "{n}"'.format(n=n) for n in [meta.classDef.name]+meta.discr ])
 
-    def generateClass(self, meta):
-        out = """<?php
-
-namespace {conf.namespace};
-
+    def _genClassDoc(self, metas, meta):
+        return """
 /**
- * @Entity"""
- 
-        if len(meta.discr):
-            out +=  """
- * @InheritanceType("{conf.inheritance}")
+ * @Entity{discr}""".format(discr="""
+ * @InheritanceType("{inheritance}")
  * @DiscriminatorColumn(name="discr", type="string")
- * @DiscriminatorMap({{"""
-            out += ', '.join([ '"{n}" = "{n}"'.format(n=n) for n in [meta.classDef.name]+meta.discr ])
-            out += '}})'
-        
-        out += """
- */
-class {meta.classDef.name}"""
-
-        if meta.classDef.parent:
-            out += ' extends ' + meta.classDef.parent
-            
-        out += ' {{'
-
-        if not meta.classDef.parent:
-            out += """
-    /**
+ * @DiscriminatorMap({map})""".format(inheritance=self.inheritance, map=self._genDiscrMap(metas, meta)) if len(meta.discr) else "") + """
+ */"""
+    
+    def _genClassName(self, metas, meta):
+        return "class " + meta.classDef.name + " " + (" extends " + meta.classDef.parent if meta.classDef.parent else "")
+    
+    def _genClassId(self, metas, meta):
+        return """/**
      * @Column(name="id", type="integer")
      * @Id
      * @GeneratedValue(strategy="AUTO")
@@ -97,26 +90,65 @@ class {meta.classDef.name}"""
 
     public function getId() {{
         return $this->id;
+    }} """ if not meta.classDef.parent else ""
+    
+    def _genAttrDoc(self, metas, meta, n):
+        if n.assoc is None:
+            return """/**
+     * @Column(name="{name}", type="{typeName}"{typeLength})
+     */""".format(name=n.name, typeName=n.typeName, typeLength=', length='+n.typeLength if n.typeLength else '')
+        else:
+            mappings = {'11': 'OneToOne', '1*': 'OneToMany', '*1': 'ManyToOne', '**': 'ManyToMany'}
+            return """/**
+     * @{mapping}(targetEntity="{target}"{mappedBy}{inversedBy})
+     */""".format(mapping=mappings[n.assoc], target=n.typeName, mappedBy=", mappedBy=\"{field}\"".format(field=n.mappedBy) if n.mappedBy else "",
+                  inversedBy=", inversedBy=\"{field}\"".format(field=n.inversedBy) if n.inversedBy else "")
+    
+    def _genAttrDec(self, metas, meta, n):
+        return "private ${name};".format(name=n.name)
+    
+    def _genAttrGetter(self, metas, meta, n):
+        return """
+    public function get{capName}() {{
+        return $this->{name};
+    }}""".format(capName=n.name[0].upper()+n.name[1:], name=n.name)
+    
+    def _genAttrSetter(self, metas, meta, n):
+        return """
+    public function set{capName}(${name}) {{
+        $this->{name} = ${name};
+    }}""".format(capName = n.name[0].upper()+n.name[1:], name=n.name)
+    
+    def _genClassAttrs(self, metas, meta):
+        return """{id}
+        """.format(id=self._genClassId(metas, meta)) + "".join([ """
+    {doc}
+    {dec}
+    {getter}
+    {setter}
+    """.format(doc=self._genAttrDoc(metas, meta, n), dec=self._genAttrDec(metas, meta, n), getter=self._genAttrGetter(metas, meta, n), setter=self._genAttrSetter(metas, meta, n))
+    for n in meta.classDef.attributes.values() ])
+    
+    def _genClassConstructor(self, metas, meta):
+        body = "".join(["$this->{name} = new \Doctrine\Common\Collections\ArrayCollection();".format(name=n.name) for n in meta.classDef.attributes.values() if n.assoc and n.assoc[1] == '*'])
+        return """public function __construct() {{
+        {body}
     }}
-    """
-        
-        out += ''.join([ """
-    /**
-     * @Column(name="{attribute.name}", type="{attribute.typeName}"{typeLength})
-     */
-    private ${attribute.name};
-
-    public function get{capitalized}() {{{{
-        return $this->{attribute.name};
-    }}}}
-
-    public function set{capitalized}(${attribute.name}) {{{{
-        $this->{attribute.name} = ${attribute.name};
-    }}}}
-    """.format(attribute=n, capitalized=n.name[0].upper()+n.name[1:], typeLength=', length='+n.typeLength if n.typeLength else '') for n in meta.classDef.attributes ])
-        
-        out += """
+        """.format(body=body) if len(body) else ""
+    
+    def _genEntity(self, metas, meta):
+        return """{doc}
+{name} {{
+    {attributes}
+    {constructor}
 }}
-"""
+        """.format(doc=self._genClassDoc(metas, meta), name=self._genClassName(metas, meta), attributes=self._genClassAttrs(metas, meta), constructor=self._genClassConstructor(metas, meta))
+
+    def generateClass(self, metas, meta):
+        template = """<?php
+
+{namespace}
+{entity}
+        """
         
-        return out.format(conf=self, meta=meta)
+        return template.format(namespace=self._genNamespace(metas, meta), entity=self._genEntity(metas, meta))
